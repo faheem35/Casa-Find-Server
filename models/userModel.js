5a06cfa5fcb7871a749abf8003501f89bd46004c
Faheem Abdulla <faheemkr7@gmail.com>
2025-07-09 11:51:05 +0400

    fourth commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,3 @@
-// const mongoose = require('mongoose')
-
-// const userSchema= new mongoose.Schema({
-//           username:{
-//                     type:String,
-//                     required:true,
-                    
-//           },
-//           email:{
-//                     type:String,
-//                     required:true,
-//                     unique:true
-//           },
-//           password:{
-//                     type:String,
-//                     required:true
-//           },
-//           profilepic:{
-//                     type:String
-                    
-//           },
-//            isAdmin:{
-//         type:Boolean,
-//         default:false
-//     },
-
-// })
-
-// const users= mongoose.model("users",userSchema)
-
-// module.exports = users
-
-
 
 const mongoose = require('mongoose');
 
